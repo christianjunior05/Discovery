93d661c626225bb4b527e6e1249e6eb51bd30250
christianjunior05 <bchrist.junior@gmail.com>
2024-09-12 00:54:37 +0000

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,16 @@ module.exports = {
         // Ajouter la police Roboto
         roboto: ['Roboto' , "system-ui"],
       },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
     },
   },
   plugins: [],
+}
 }
